abb8657806f9b29f396addffc458d06b7b3e08b2
David <95488893+DWAcademProgramming@users.noreply.github.com>
2022-11-08 14:05:53 -0600

    More features added.

--- feedback/feedback/src/components/FeedbackItem.jsx
+++ feedback/feedback/src/components/FeedbackItem.jsx
@@ -1,11 +1,11 @@
 import React, {useContext} from 'react'
-import {FaTimes} from 'react-icons/fa'
+import {FaTimes, FaEdit} from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackItem({item}) {
-    const {deleteFeedback} = useContext(FeedbackContext)
+    const {deleteFeedback, editFeedback} = useContext(FeedbackContext)
     const handleClick =(id)=>{
         console.log(id)
     }
@@ -21,6 +21,12 @@ function FeedbackItem({item}) {
         >
             <FaTimes color='purple' />
         </button>
+        <bitton 
+            onClick={() => editFeedback(item)}
+            className="edit"
+        >
+            <FaEdit color='purple' /> 
+        </bitton>
         <div className="text-display">
             {item.text}
         </div>
