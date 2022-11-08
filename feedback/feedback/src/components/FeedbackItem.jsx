f64db2147e0c688de27b298750f121fdea82ebe6
gntpldes <h3c6j6@u.northwestern.edu>
2022-11-08 13:05:04 -0600

    Looks like I've hit the laptop limit...

--- feedback/feedback/src/components/FeedbackItem.jsx
+++ feedback/feedback/src/components/FeedbackItem.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {FaTimes} from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackItem({item, handleDelete}) {
+function FeedbackItem({item}) {
+    const {deleteFeedback} = useContext(FeedbackContext)
     const handleClick =(id)=>{
         console.log(id)
     }
@@ -14,7 +16,7 @@ function FeedbackItem({item, handleDelete}) {
             {item.rating}
         </div>
         <button 
-            onClick={()=>handleDelete(item.id)} 
+            onClick={()=>deleteFeedback(item.id)} 
             className="close"
         >
             <FaTimes color='purple' />
--- feedback/feedback/src/components/FeedbackList.jsx
+++ feedback/feedback/src/components/FeedbackList.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
-import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({feedback, handleDelete}) {
+function FeedbackList() {
+    const {feedback} = useContext(FeedbackContext)
+
     if(!feedback || feedback.length === 0 ){
         return <p>No feedback</p>
     }
@@ -20,23 +22,12 @@ function FeedbackList({feedback, handleDelete}) {
                     <FeedbackItem 
                         key={item.id} 
                         item={item} 
-                        handleDelete={handleDelete}
                     />
                 </motion.div>
             ))}
         </AnimatePresence>
     </div>
   )
-}
-
-FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired, 
-            rating: PropTypes.number.isRequired
-        })
-    )
 }
 
 export default FeedbackList
