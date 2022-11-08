af65944943424fb61c4bd5cb27ddf1c4b973489d
gntpldes <h3c6j6@u.northwestern.edu>
2022-11-08 06:52:15 -0600

    Fixed the bug and now it's working...

--- feedback/feedback/src/components/FeedbackItem.jsx
+++ feedback/feedback/src/components/FeedbackItem.jsx
@@ -4,14 +4,19 @@ import PropTypes from 'prop-types'
 import Card from './shared/Card'
 
 function FeedbackItem({item, handleDelete}) {
-
+    const handleClick =(id)=>{
+        console.log(id)
+    }
 
   return (
     <Card>
         <div className="num-display">
             {item.rating}
         </div>
-        <button onClick={} className="close">
+        <button 
+            onClick={()=>handleDelete(item.id)} 
+            className="close"
+        >
             <FaTimes color='purple' />
         </button>
         <div className="text-display">
