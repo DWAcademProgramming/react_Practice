72592b589d62ccf6ecc472b7e62cd2dc09a97850
gntpldes <h3c6j6@u.northwestern.edu>
2022-11-15 13:49:24 -0600

    Lets be cautious here...

--- house-marketplace/src/App.js
+++ house-marketplace/src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Navbar from './components/Navbar'
+import PrivateRoutes from './components/PrivateRoutes'
 import Explore from './pages/Explore'
 import ForgotPassword from './pages/ForgotPassword'
 import Offers from './pages/Offers'
@@ -14,14 +17,16 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<PrivateRoutes />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
           <Route path='/signin' element={<SignIn />} />
           <Route path='/signup' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
         <Navbar />
       </Router>
-      
+      <ToastContainer />
     </>
   );
 }
