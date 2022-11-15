bf37eed125ecce2ee62ad808bf3062c4bf02dd38
gntpldes <h3c6j6@u.northwestern.edu>
2022-11-15 17:02:28 -0600

    WTF?!!

--- house-marketplace/src/pages/Categories.jsx
+++ house-marketplace/src/pages/Categories.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+import {
+    collection,
+    getDocs, 
+    query, 
+    where, 
+    orderBy, 
+    limit, 
+    startAfter
+} from 'firebase/firestore'
+import {db} from '../firebase.config'
+import {toast} from 'react-toastify'
+import Spinner from '../components/Spinner'
+import { async } from '@firebase/util'
 
 function Categories() {
+    const [listings, setListings] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const params = useParams();
+
+    useEffect(() => {
+        const fetchListings = async() =>{
+            try {
+                //Get reference
+                const listingRef = collection(db, 'listings')
+
+                //Create a query
+                const q = query(
+                    listingRef, 
+                    where('type', '==', params.categoryName), 
+                    orderBy('timestamp', 'desc'), 
+                    limit(10)
+                )
+                //Execute query
+                const querySnap = await getDocs(q)
+
+                let listings = []
+
+                querySnap.forEach((doc) =>{
+                    console.log(doc.data())
+                })
+
+                setListings(listings)
+                setLoading(false)
+            } catch (error) {
+                toast.error('Could not fetch listings')
+            }
+        }
+    }, [])
   return (
-    <div>Categories</div>
+    <h1>Category</h1>
   )
 }
 
